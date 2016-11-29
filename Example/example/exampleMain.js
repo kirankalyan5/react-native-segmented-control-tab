10005d12bbf8946e8e3273b7814a6b3c7b77aa90
Kiran <Kiran.k@thoughtworks.com>
2016-11-29 20:06:13 +0530

    Updated the example to used the usage of index

--- Example/example/exampleMain.js
+++ Example/example/exampleMain.js
@@ -8,19 +8,39 @@ import {
 import SegmentedControlTab from 'react-native-segmented-control-tab'
 
 class ExampleMain extends Component {
+   constructor(props) {
+        super(props)
+        this.handleSegmentChange = this.handleSegmentChange.bind(this)
+        this.state = {
+            secondTab: false
+        }
+    }
+  handleSegmentChange(index) {
+        this.setState({
+            secondTab: index == 1
+        })
+         console.log(index)
+    }
   render() {
     return (
       <View style={styles.container}>
             <Text>Default segmented control</Text>
             <SegmentedControlTab />
-            <Text>Custom segmented control</Text>
+            <Text>Custom segmented control with custom styles</Text>
             <SegmentedControlTab values={['one', 'two']}
+                onTabPress= {index => this.handleSegmentChange(index) }
                 borderRadius={0}
                 tabsContainerStyle={{height: 50, backgroundColor: '#F2F2F2'}}
                 tabStyle={{backgroundColor: '#F2F2F2', borderWidth: 0}}
                 activeTabStyle={{backgroundColor: 'white', marginTop: 2}}
                 tabTextStyle={{color: '#444444', fontWeight: 'bold'}}
                 activeTabTextStyle={{color: '#888888'}}/>
+                {!this.state.secondTab &&
+                  <Text style={styles.tabViewText}>First tab!!</Text>
+                }
+                 {this.state.secondTab &&
+                  <Text style={styles.tabViewText}>Second tab!!</Text>
+                }
       </View>
     );
   }
@@ -33,6 +53,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
     padding: 10
+  },
+  tabViewText: {
+    color: '#444444',
+    fontWeight: 'bold',
+    marginTop: 50,
+    fontSize: 18
   }
 });
 
