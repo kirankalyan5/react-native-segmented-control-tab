4815b89908202f65c0c74b1e2fc7a222d36c001b
Kiran <kiransinbox1@gmail.com>
2017-07-04 08:22:43 +0530

    Made the tab text to be 1 liner and added ellipsize for longer text

--- SegmentedControlTab.js
+++ SegmentedControlTab.js
@@ -35,9 +35,11 @@ const TabOption = ({
             onPress={() => onTabPress(index)}
             activeOpacity={1}>
             <Text style={[
-                styles.textStyle,
+                styles.tabTextStyle,
                 tabTextStyle,
-                isTabActive ? [styles.activeTabTextStyle, activeTabTextStyle] : {}]}>
+                isTabActive ? [styles.activeTabTextStyle, activeTabTextStyle] : {}]}
+                numberOfLines={1}
+                ellipsizeMode="tail">
                 {text}
             </Text>
         </TouchableOpacity>
