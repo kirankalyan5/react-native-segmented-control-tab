1d19ee0df2a6fb6981cff0c980a1633c6c405cb4
Kyle Fang <zhigang1992@gmail.com>
2018-11-06 20:05:27 +0800

    Add import React in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { StyleProp, ViewStyle, TextStyle } from "react-native";
 export interface SegmentedControlTabProperties extends React.Props<ReactNativeSegmentedControlTab> {
     // Default ['One', 'Two', 'Three'] ; titles of tabs Default 
